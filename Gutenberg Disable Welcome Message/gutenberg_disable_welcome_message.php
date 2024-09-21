9dafab17fb8d072d733e895a024ff56d55eafe88
meksONE <meksone@gmail.com>
2024-09-21 09:52:47 +0200

    new snippet

---
+++ Gutenberg Disable Welcome Message/gutenberg_disable_welcome_message.php
@@ -0,0 +1,18 @@
+<?php
+$version = "<!#FV> 0.0.1 </#FV>";
+
+/**
+ * More info https://www.wpexplorer.com/disable-welcome-to-the-block-editor/
+ */
+function mk_disable_editor_welcome_message() {
+	?>
+	<script>
+		window.onload = (event) => {
+			wp.data && wp.data.select( 'core/edit-post' ).isFeatureActive( 'welcomeGuide' ) && wp.data.dispatch( 'core/edit-post' ).toggleFeature( 'welcomeGuide' );
+		};
+	</script>
+	<?php
+}
+add_action( 'admin_head-post.php', 'mk_disable_editor_welcome_message' );
+add_action( 'admin_head-post-new.php',  'mk_disable_editor_welcome_message' );
+add_action( 'admin_head-edit.php', 'mk_disable_editor_welcome_message' );
