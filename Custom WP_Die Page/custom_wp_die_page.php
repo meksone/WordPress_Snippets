56f642034b7e37525cade86198a16007dc440a0f
meksONE <meksone@gmail.com>
2024-09-22 21:26:54 +0200

    update snippets

--- Custom WP_Die Page/custom_wp_die_page.php
+++ Custom WP_Die Page/custom_wp_die_page.php
@@ -6,15 +6,15 @@ $version = "<!#FV> 0.0.1 </#FV>";
  */
 
 // custom die handler
-function wpmu_get_custom_die_handler() {
-    return 'wpmu_custom_css_on_wp_die';
+function mk_get_custom_die_handler() {
+    return 'mk_custom_css_on_wp_die';
 }
 
 // hook the function
-add_filter('wp_die_handler', 'wpmu_get_custom_die_handler' );
+add_filter('wp_die_handler', 'mk_get_custom_die_handler' );
 
 // custom login for theme
-function wpmu_custom_css_on_wp_die($message, $title = '', $args = array() ) {
+function mk_custom_css_on_wp_die($message, $title = '', $args = array() ) {
 	//$defaults = array( 'ciao' => 403 );
 	$r = wp_parse_args($args);
 	//$r['response']; //output error code response
--- Remove Menu Items/remove_menu_items.php
+++ Remove Menu Items/remove_menu_items.php
@@ -1,7 +1,11 @@
 <?php
-$version = "<!#FV> 0.0.5 </#FV>";
 
-$user_to_exclude = array('bim-adm');
+$version = "<!#FV> 0.0.7 </#FV>";
+
+$user_to_exclude = array('bim-adma');
+
+// Global variable to store removed pages
+$removed_pages = array();
 
 $menus_to_disable = array(
     (object) array('menuname' => 'WP Options - Main', 'mainmenu' => 'options-general.php', 'exclude' => $user_to_exclude), 																
@@ -21,47 +25,61 @@ $menus_to_disable = array(
 	(object) array('menuname' => 'Plugins - Editor', 'mainmenu' => 'plugins.php', 'submenu' => 'plugin-editor.php', 'exclude' => $user_to_exclude), 									
 	(object) array('menuname' => 'JAMP Notes', 'mainmenu' => 'edit.php?post_type=jamp_note', 'exclude' => $user_to_exclude),														
 	(object) array('menuname' => 'WP Options - Jamp Notes Plugin', 'mainmenu' => 'options-general.php', 'submenu' => 'jamp_options', 'exclude' => $user_to_exclude), 									
-	(object) array('menuname' => 'Elementor - Main', 'mainmenu' => 'elementor', 'exclude' => $user_to_exclude),																			
+	(object) array('menuname' => 'Elementor - Main', 'mainmenu' => 'admin.php?page=elementor', 'exclude' => $user_to_exclude),																			
 	(object) array('menuname' => 'Elementor - Template Library', 'mainmenu' => 'edit.php?post_type=elementor_library', 'exclude' => $user_to_exclude),												
-	(object) array('menuname' => 'Elementor - License', 'mainmenu' => 'elementor', 'submenu' => 'elementor-license', 'exclude' => $user_to_exclude), 										
-	(object) array('menuname' => 'Elementor - Apps ', 'mainmenu' => 'elementor', 'submenu' => 'elementor-apps', 'exclude' => $user_to_exclude), 											
-	(object) array('menuname' => 'Elementor - Role manager ', 'mainmenu' => 'elementor', 'submenu' => 'elementor-role-manager', 'exclude' => $user_to_exclude), 									
-	(object) array('menuname' => 'Elementor - Settings ', 'mainmenu' => 'elementor', 'submenu' => 'elementor-settings', 'exclude' => $user_to_exclude), 										
-	(object) array('menuname' => 'Elementor - Tools ', 'mainmenu' => 'elementor', 'submenu' => 'elementor-tools', 'exclude' => $user_to_exclude), 										
-	(object) array('menuname' => 'Elementor - Info ', 'mainmenu' => 'elementor', 'submenu' => 'elementor-system-info', 'exclude' => $user_to_exclude), 									
-	(object) array('menuname' => 'Elementor - Knowledge Base', 'mainmenu' => 'elementor', 'submenu' => 'go_knowledge_base_site', 'exclude' => $user_to_exclude), 									
-	(object) array('menuname' => 'Dynamic Content for Elementor - Main', 'mainmenu' => 'dce-features', 'exclude' => $user_to_exclude),    																	
-	(object) array('menuname' => 'Dynamic Content for Elementor - License ', 'mainmenu' => 'dce-features', 'submenu' => 'dce-license', 'exclude' => $user_to_exclude),                            				
-	(object) array('menuname' => 'Dynamic Content for Elementor - HTML Templates', 'mainmenu' => 'dce-features','submenu' => 'edit.php?post_type=dce_html_template', 'exclude' => $user_to_exclude),    				
-	(object) array('menuname' => 'LoginPress - Main', 'mainmenu' => 'loginpress-settings', 'exclude' => $user_to_exclude),                  					  						 	
-	(object) array('menuname' => 'LoginPress - License', 'mainmenu' => 'loginpress-settings','submenu' => 'loginpress-license', 'exclude' => $user_to_exclude),               				
-	(object) array('menuname' => 'LoginPress - Import/Export', 'mainmenu' => 'loginpress-settings','submenu' => 'loginpress-import-export', 'exclude' => $user_to_exclude),        	 			
-	(object) array('menuname' => 'LoginPress - Help', 'mainmenu' => 'loginpress-settings','submenu' => 'loginpress-help', 'exclude' => $user_to_exclude),              			    	
-	(object) array('menuname' => 'LoginPress - Addons', 'mainmenu' => 'loginpress-settings','submenu' => 'loginpress-addons', 'exclude' => $user_to_exclude),           				    
+	(object) array('menuname' => 'Elementor - License', 'mainmenu' => 'admin.php?page=elementor', 'submenu' => 'elementor-license', 'exclude' => $user_to_exclude), 										
+	(object) array('menuname' => 'Elementor - Apps ', 'mainmenu' => 'admin.php?page=elementor', 'submenu' => 'elementor-apps', 'exclude' => $user_to_exclude), 											
+	(object) array('menuname' => 'Elementor - Role manager ', 'mainmenu' => 'admin.php?page=elementor', 'submenu' => 'elementor-role-manager', 'exclude' => $user_to_exclude), 									
+	(object) array('menuname' => 'Elementor - Settings ', 'mainmenu' => 'admin.php?page=elementor', 'submenu' => 'elementor-settings', 'exclude' => $user_to_exclude), 										
+	(object) array('menuname' => 'Elementor - Tools ', 'mainmenu' => 'admin.php?page=elementor', 'submenu' => 'elementor-tools', 'exclude' => $user_to_exclude), 										
+	(object) array('menuname' => 'Elementor - Info ', 'mainmenu' => 'admin.php?page=elementor', 'submenu' => 'elementor-system-info', 'exclude' => $user_to_exclude), 									
+	(object) array('menuname' => 'Elementor - Knowledge Base', 'mainmenu' => 'admin.php?page=elementor', 'submenu' => 'go_knowledge_base_site', 'exclude' => $user_to_exclude), 									
+	(object) array('menuname' => 'Dynamic Content for Elementor - Main', 'mainmenu' => 'admin.php?page=dce-features', 'exclude' => $user_to_exclude),    																	
+	(object) array('menuname' => 'Dynamic Content for Elementor - License ', 'mainmenu' => 'admin.php?page=dce-features', 'submenu' => 'dce-license', 'exclude' => $user_to_exclude),                            				
+	(object) array('menuname' => 'Dynamic Content for Elementor - HTML Templates', 'mainmenu' => 'admin.php?page=dce-features','submenu' => 'edit.php?post_type=dce_html_template', 'exclude' => $user_to_exclude),    				
+	(object) array('menuname' => 'LoginPress - Main', 'mainmenu' => 'admin.php?page=loginpress-settings', 'exclude' => $user_to_exclude),                  					  						 	
+	(object) array('menuname' => 'LoginPress - License', 'mainmenu' => 'admin.php?page=loginpress-settings','submenu' => 'loginpress-license', 'exclude' => $user_to_exclude),               				
+	(object) array('menuname' => 'LoginPress - Import/Export', 'mainmenu' => 'admin.php?page=loginpress-settings','submenu' => 'loginpress-import-export', 'exclude' => $user_to_exclude),        	 			
+	(object) array('menuname' => 'LoginPress - Help', 'mainmenu' => 'admin.php?page=loginpress-settings','submenu' => 'loginpress-help', 'exclude' => $user_to_exclude),              			    	
+	(object) array('menuname' => 'LoginPress - Addons', 'mainmenu' => 'admin.php?page=loginpress-settings','submenu' => 'loginpress-addons', 'exclude' => $user_to_exclude),           				    
 	(object) array('menuname' => 'LoginPress - submenu in Themes', 'mainmenu' => 'themes.php','submenu' => 'abw', 'exclude' => $user_to_exclude),            											
 	(object) array('menuname' => 'ACF Pro - Main', 'mainmenu' => 'edit.php?post_type=acf-field-group', 'exclude' => $user_to_exclude),                       							
 	(object) array('menuname' => 'ACF Pro - Tools', 'mainmenu' => 'edit.php?post_type=acf-field-group','submenu' => 'acf-tools', 'exclude' => $user_to_exclude),         				
 	(object) array('menuname' => 'ACF Pro - Updates/License', 'mainmenu' => 'edit.php?post_type=acf-field-group','submenu' => 'acf-settings-updates', 'exclude' => $user_to_exclude),			
-	(object) array('menuname' => 'Duplicator - Main', 'mainmenu' => 'duplicator', 'exclude' => $user_to_exclude),    																	
-	(object) array('menuname' => 'Duplicator - Informations', 'mainmenu' => 'duplicator','submenu' => 'duplicator-about-us', 'exclude' => $user_to_exclude),										
-	(object) array('menuname' => 'Duplicator - Storage', 'mainmenu' => 'duplicator','submenu' => 'duplicator-storage', 'exclude' => $user_to_exclude),										
-	(object) array('menuname' => 'Duplicator - Update to Pro', 'mainmenu' => 'duplicator','submenu' => 'https://duplicator.com/lite-upgrade/?utm_medium=admin-menu&utm_content=Upgrade+to+Pro&utm_source=WordPress&utm_campaign=liteplugin', 'exclude' => $user_to_exclude),
-	(object) array('menuname' => 'PAFE - Main', 'mainmenu' => 'premium-addons', 'exclude' => $user_to_exclude),    																
-	(object) array('menuname' => 'PAFE - License', 'mainmenu' => 'premium-addons','submenu' => 'premium-addons#tab=license', 'exclude' => $user_to_exclude),							
-	(object) array('menuname' => 'PAFE - White Label', 'mainmenu' => 'premium-addons','submenu' => 'premium-addons#tab=white-label', 'exclude' => $user_to_exclude),						
-	(object) array('menuname' => 'PAFE - System Info', 'mainmenu' => 'premium-addons','submenu' => 'premium-addons#tab=system-info', 'exclude' => $user_to_exclude),						
-	(object) array('menuname' => 'PAFE - Version Control', 'mainmenu' => 'premium-addons','submenu' => 'premium-addons#tab=vcontrol', 'exclude' => $user_to_exclude),							
-	(object) array('menuname' => 'PAFE - Integrations', 'mainmenu' => 'premium-addons','submenu' => 'premium-addons#tab=integrations', 'exclude' => $user_to_exclude),						
+	(object) array('menuname' => 'Duplicator - Main', 'mainmenu' => 'admin.php?page=duplicator', 'exclude' => $user_to_exclude),    																	
+	(object) array('menuname' => 'Duplicator - Informations', 'mainmenu' => 'admin.php?page=duplicator','submenu' => 'duplicator-about-us', 'exclude' => $user_to_exclude),										
+	(object) array('menuname' => 'Duplicator - Storage', 'mainmenu' => 'admin.php?page=duplicator','submenu' => 'duplicator-storage', 'exclude' => $user_to_exclude),										
+	(object) array('menuname' => 'Duplicator - Update to Pro', 'mainmenu' => 'admin.php?page=duplicator','submenu' => 'https://duplicator.com/lite-upgrade/?utm_medium=admin-menu&utm_content=Upgrade+to+Pro&utm_source=WordPress&utm_campaign=liteplugin', 'exclude' => $user_to_exclude),
+	(object) array('menuname' => 'PAFE - Main', 'mainmenu' => 'admin.php?page=premium-addons', 'exclude' => $user_to_exclude),    																
+	(object) array('menuname' => 'PAFE - License', 'mainmenu' => 'admin.php?page=premium-addons','submenu' => 'premium-addons#tab=license', 'exclude' => $user_to_exclude),							
+	(object) array('menuname' => 'PAFE - White Label', 'mainmenu' => 'admin.php?page=premium-addons','submenu' => 'premium-addons#tab=white-label', 'exclude' => $user_to_exclude),						
+	(object) array('menuname' => 'PAFE - System Info', 'mainmenu' => 'admin.php?page=premium-addons','submenu' => 'premium-addons#tab=system-info', 'exclude' => $user_to_exclude),						
+	(object) array('menuname' => 'PAFE - Version Control', 'mainmenu' => 'admin.php?page=premium-addons','submenu' => 'premium-addons#tab=vcontrol', 'exclude' => $user_to_exclude),							
+	(object) array('menuname' => 'PAFE - Integrations', 'mainmenu' => 'admin.php?page=premium-addons','submenu' => 'premium-addons#tab=integrations', 'exclude' => $user_to_exclude),						
 	(object) array('menuname' => 'Options - WP Folder Plugin', 'mainmenu' => 'options-general.php','submenu' => 'option-folder', 'exclude' => $user_to_exclude),									
 	(object) array('menuname' => 'Options - Redis Plugin', 'mainmenu' => 'options-general.php','submenu' => 'redis-cache', 'exclude' => $user_to_exclude),									
 	(object) array('menuname' => 'Options - Disable Comments Plugin', 'mainmenu' => 'options-general.php','submenu' => 'disable_comments_settings', 'exclude' => $user_to_exclude),						
 	(object) array('menuname' => 'Options - Admin Columns Pro', 'mainmenu' => 'options-general.php','submenu' => 'codepress-admin-columns', 'exclude' => $user_to_exclude),						
-	(object) array('menuname' => 'Code Snippets - Main', 'mainmenu' => 'snippets', 'exclude' => $user_to_exclude),    																		
-	(object) array('menuname' => 'Code Snippets - XXX', 'mainmenu' => 'snippets','submenu' => 'code-snippets-welcome', 'exclude' => $user_to_exclude),										
+	(object) array('menuname' => 'Code Snippets - Main', 'mainmenu' => 'admin.php?page=snippets', 'exclude' => $user_to_exclude),    																		
+	(object) array('menuname' => 'Code Snippets - XXX', 'mainmenu' => 'admin.php?page=snippets','submenu' => 'code-snippets-welcome', 'exclude' => $user_to_exclude),										
 );
 
 
 function disable_menu_items($options) {
+    global $removed_pages;
+	
+	// Clean menu items URL
+	$stripFromURL = array( '	edit.php?post_type=',
+								'admin.php?page=',
+								'site-editor.php?path=',
+								'customize.php?return=',
+								'themes.php?page=',
+								'tools.php?page=',
+								'edit-tags.php?taxonomy=',
+						);
+	
+	//echo "-------------------------------------------- clean array: " . $stripFromURL . "<br>";
+
     // Get the current user
     $current_user = wp_get_current_user();
 
@@ -74,12 +92,62 @@ function disable_menu_items($options) {
                 // Check if the submenu is set
                 if(isset($option->submenu)) {
                     // Remove the submenu page
-                    remove_submenu_page($option->mainmenu, $option->submenu);
+                    // Clean menu and submenu string
+                    $cleanMenuitem = stripFromArray($option->mainmenu, $stripFromURL);
+					$cleanSubMenuitem = stripFromArray($option->submenu, $stripFromURL);
+					
+					//echo "------------------------------------------- mainmenu page: " . $cleanMenuitem . "<br>";
+					//echo "------------------------------------------- submenu page: " . $cleanSubMenuitem . "<br>";
+					
+                    remove_submenu_page($cleanMenuitem, $cleanSubMenuitem);
+
+                    // Add the submenu page to the removed pages - not cleaned!
+                    $removed_pages[] = $option->submenu;
                 } else {
                     // Remove the main menu page
-                    remove_menu_page($option->mainmenu);
+                    // Clean menu string
+                    $cleanMenuitem = stripFromArray($option->mainmenu, $stripFromURL);
+                    remove_menu_page($cleanMenuitem);
+
+                    // Add the main menu page to the removed pages
+                    $removed_pages[] = $option->mainmenu;
                 }
             }
+        }
+    }
+	/* Block access with 403 error */
+	redirect_removed_pages();
+}
+
+/* Helper function
+ * clean a string using array as input
+ * */
+function stripFromArray($string, $array) {
+    foreach($array as $item) {
+        $string = str_replace($item, '', $string);
+    }
+    return $string;
+}
+
+function redirect_removed_pages() {
+    global $removed_pages;
+
+    // Get the current page URL
+    $current_page_url = $_SERVER['REQUEST_URI'];
+	//echo "------------------------------------------- removed page: " . $removed_page;
+	//echo "------------------------------------------------------ current page: " . $current_page_url . "<br>";
+
+    // Split the current page URL into its components
+    $current_page_url_components = explode('/', trim($current_page_url, '/'));
+
+    // Check if the current page URL contains any of the removed pages
+    foreach($removed_pages as $removed_page) {
+        if(in_array($removed_page, $current_page_url_components)) {
+			//echo "------------------------------------------- removed page: " . $removed_page . "<br>";
+			//echo "------------------------------------------------------ current page: " . $current_page_url . "<br>";
+            // Return a 403 error
+            status_header( 403 );
+            wp_die( 'Access denied - you can\'t reach this page' );
         }
     }
 }
@@ -87,3 +155,27 @@ function disable_menu_items($options) {
 add_action('admin_menu', function() use ($menus_to_disable) {
     disable_menu_items($menus_to_disable);
 },999);
+
+// Hook into the 'template_redirect' action
+//add_action('template_redirect', 'redirect_removed_pages');
+
+
+/*
+ * Print WP Dashboard Menus and Submenus
+ * Output the array on Dashboard main page
+ * reference https://wordpress.stackexchange.com/questions/148973/how-can-i-get-an-array-list-of-all-current-wordpress-admin-menu-items
+ * */
+if(false){
+	if (!function_exists('debug_admin_menus')):
+	function debug_admin_menus() {
+		global $submenu, $menu, $pagenow;
+		if ( current_user_can('manage_options') ) { // Only for admins
+			if( $pagenow == 'index.php' ) {  // Print in dashboard
+				echo '<pre>'; print_r( $menu ); echo '</pre>'; // TOP LEVEL MENUS
+				echo '<pre>'; print_r( $submenu ); echo '</pre>'; // SUBMENUS
+			}
+		}
+	}
+	add_action( 'admin_notices', 'debug_admin_menus' );
+	endif;
+	};
