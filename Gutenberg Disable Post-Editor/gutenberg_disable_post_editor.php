84a94c5a3080a3d2da35216a6f6d8d11696d2991
meksONE <meksone@gmail.com>
2024-09-26 12:54:49 +0200

    update snippet, add new snippet

--- Gutenberg Disable Post-Editor/gutenberg_disable_post_editor.php
+++ Gutenberg Disable Post-Editor/gutenberg_disable_post_editor.php
@@ -1,5 +1,6 @@
 <?php
-$version = "<!#FV> 0.0.2 </#FV>";
+
+$version = "<!#FV> 0.0.3 </#FV>";
 
 /*
  * Disable all Gutenberg blocks for certain post types
@@ -12,11 +13,26 @@ add_filter('allowed_block_types', 'mk_disable_all_blocks', 10, 2);
 
 function mk_disable_all_blocks($allowed_blocks, $post) {
     // Define the post types where Gutenberg blocks should be disabled
-    $postTypes = array('film');
+    $postTypes = array('corso-master', 'corsi');
+
+    // If the current post type is in the list, disable Gutenberg blocks
+    /*if (in_array($post->post_type, $postTypes)) {
+		add_action( 'acf/input/admin_footer', 'mk_fix_post_content_height' );
+        return array();
+    }
+    */
 
     // If the current post type is in the list, disable Gutenberg blocks
     if (in_array($post->post_type, $postTypes)) {
-		add_action( 'acf/input/admin_footer', 'mk_fix_post_content_height' );
+        // Check if the ACF action exists
+        if (has_action('acf/input/admin_footer')) {
+            // If it exists, add the action
+            add_action('acf/input/admin_footer', 'mk_fix_post_content_height');
+        } else {
+            // If it doesn't exist, use a standard WordPress action
+            // Replace 'standard_wp_action' with the actual action you want to use
+            add_action('admin_head', 'mk_fix_post_content_height');
+        }
         return array();
     }
 
@@ -25,12 +41,19 @@ function mk_disable_all_blocks($allowed_blocks, $post) {
 }
 
 // Fix the post content height
-function mk_fix_post_content_height(){
-    // Define the CSS styles
-    $styles = "
+function mk_fix_post_content_height(){ 
+	?>
+    <style>
+    
+	/* hide editor space */
     :root :where(.editor-styles-wrapper)::after {
-        height: unset;
+        height: unset!important;
     }
+	
+	/* hide add button */
+	button#\:r1\: {
+    display: none;
+	}
     .editor-visual-editor.edit-post-visual-editor {
         max-height: 150px;
     }
@@ -40,8 +63,7 @@ function mk_fix_post_content_height(){
     }
     .editor-visual-editor__post-title-wrapper.edit-post-visual-editor__post-title-wrapper {
         margin-top: 1px !important;
-    }
-    ";
-    // Print the styles in the page
-    echo "<style>$styles</style>";
+    }						
+	</style>
+		<?php
 }
---
+++ Gutenberg Remove Taxonomy From Edit Screen/gutenberg_remove_taxonomy_from_edit_screen.php
@@ -0,0 +1,41 @@
+<?php
+
+$version = "<!#FV> 0.0.1 </#FV>";
+
+function mk_remove_taxonomy_from_edit_screen()
+{
+    // Get the current screen
+    $screen = get_current_screen();
+
+    // Check if we're on the edit screen for the 'corsi' CPT
+    if ('corsi' === $screen->post_type && 'post' === $screen->base) {
+?>
+        <script type="text/javascript">
+            document.addEventListener('DOMContentLoaded', function() {
+                // ASCII Art
+                var asciiArt = `
+%c
+
+░▒▓██████████████▓▒░░▒▓████████▓▒░▒▓█▓▒░░▒▓█▓▒░░▒▓███████▓▒░░▒▓██████▓▒░░▒▓███████▓▒░░▒▓████████▓▒░ 
+░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░        
+░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░        
+░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓██████▓▒░ ░▒▓███████▓▒░ ░▒▓██████▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓██████▓▒░   
+░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░        
+░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░        
+░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓████████▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓███████▓▒░ ░▒▓██████▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓████████▓▒░ 
+                                                                                                    
+                                                                                                    
+
+                                           
+`;
+                console.log(asciiArt, 'color: #00ff00; background: #000; font-family: monospace;');
+                console.log('%cRemoving the taxonomy panel Tag Corso for corsi CPT', 'color: #00ff00; background: #000; font-family: monospace; font-weight: bold;');
+                console.log('%cRemoving the taxonomy panel Categorie Corso for corsi CPT', 'color: #00ff00; background: #000; font-family: monospace; font-weight: bold;');
+                wp.data.dispatch('core/edit-post').removeEditorPanel('taxonomy-panel-categoria-corso');
+                wp.data.dispatch('core/edit-post').removeEditorPanel('taxonomy-panel-tag-corso');
+            });
+        </script>
+<?php
+    }
+}
+add_action('admin_footer', 'mk_custom_admin_script');
