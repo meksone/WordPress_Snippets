d0b55cbed7c0917d07c9bc53634ab64ba215b907
meksONE <meksone@gmail.com>
2024-12-09 19:24:01 +0100

    update

--- Remove Dashboard Widgets/remove_dashboard_widgets.php
+++ Remove Dashboard Widgets/remove_dashboard_widgets.php
@@ -1,7 +1,7 @@
 <?php
 
 $snippet_name = "remove_dashboard_widgets";
-$version = "<!#FV> 0.0.6 </#FV>";
+$version = "<!#FV> 0.0.7 </#FV>";
 
 remove_action('welcome_panel', 'wp_welcome_panel'); // remove the welcome panel - doesn't work from 6.0 onward...
 
@@ -9,6 +9,7 @@ add_action( 'wp_dashboard_setup', 'mk_rm_meta_boxes' );
 function mk_rm_meta_boxes()
 {
 	//remove_meta_box( 'dashboard_site_health', 'dashboard', 'normal');				// Site Health widget
+	remove_action('welcome_panel', 'wp_welcome_panel');								// WordPress Welcome Panel
 	remove_meta_box( 'dashboard_right_now', 'dashboard', 'normal' ); 				// Right Now widget that tells you post/comment counts	
 	remove_meta_box( 'dashboard_recent_comments', 'dashboard', 'normal' );			// Recent comments widget
 	remove_meta_box( 'dashboard_incoming_links', 'dashboard', 'normal' );			// Incoming links widget
@@ -20,8 +21,9 @@ function mk_rm_meta_boxes()
 	remove_meta_box( 'dashboard_activity', 'dashboard', 'normal');					// Activity widget
 	remove_meta_box( 'e-dashboard-overview', 'dashboard', 'normal');				// Elementor Overview Dashboard
 	remove_meta_box( 'duplicator_dashboard_widget', 'dashboard', 'normal');			// Duplicator widget
-	remove_action('welcome_panel', 'wp_welcome_panel');								// WordPress Welcome Panel
 	remove_meta_box( 'sq_dashboard_widget', 'dashboard', 'normal');					// SqirrlySEO
 	remove_meta_box( 'wc_admin_dashboard_setup', 'dashboard', 'normal');			// Remove WooCommerce
 	remove_meta_box( 'wp_mail_smtp_reports_widget_lite', 'dashboard', 'normal');	// Remove WP Mail SMTP
+	remove_meta_box( 'pa-stories', 'dashboard', 'normal');							// Premium Addons
+	remove_meta_box( 'dashboard_rediscache', 'dashboard', 'normal');				// Redis Cache
 }
