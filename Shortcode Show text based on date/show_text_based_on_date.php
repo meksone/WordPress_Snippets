00f0c293833376a0524580c62910496a2436b845
meksONE <meksone@gmail.com>
2025-02-27 00:40:57 +0100

    Changes to be committed: modified:   Shortcode Show text based on date/show_text_based_on_date.php added check for date - if movie date is 90 days or less from today, show text, otherwise show nothing

--- Shortcode Show text based on date/show_text_based_on_date.php
+++ Shortcode Show text based on date/show_text_based_on_date.php
@@ -1,7 +1,7 @@
 <?php
 
 $snippet_name = "show_text_based_on_date";
-$version = "<!#FV> 0.0.3 </#FV>";
+$version = "<!#FV> 0.0.4 </#FV>";
 
 add_shortcode('film_date_display', 'date_display_function');
 function date_display_function($atts) {
@@ -24,7 +24,7 @@ function date_display_function($atts) {
     // Get both custom fields
     $custom_field_date = get_field('gruppo_sinistra_data_film');
     $custom_field_text = get_field('gruppo_sinistra_cappello');
-	$cf_mostra_anno = get_field('gruppo_sinistra_mostrare_anno');
+    $cf_mostra_anno = get_field('gruppo_sinistra_mostrare_anno');
 
     // If text exists and date is empty, show only text
     if (!empty($custom_field_text) && empty($custom_field_date)) {
@@ -33,24 +33,35 @@ function date_display_function($atts) {
     
     // Process the date display if it exists
     if (!empty($custom_field_date)) {
-        $custom_date_int = (int)date('Ymd', strtotime($custom_field_date));
-        $today_date_int = (int)date('Ymd', time());
-        $day = date('j', strtotime($custom_field_date));
-        $month_num = (int)date('n', strtotime($custom_field_date));
-		if( $cf_mostra_anno == 1 ){
-		$year = date('Y', strtotime($custom_field_date));
-		} else {
-		$year = "";
-		};
+        $custom_date = strtotime($custom_field_date);
+        $today = time();
+        
+        // Calculate the difference between today and the custom date
+        $difference = $today - $custom_date;
+        $days_difference = floor($difference / (24*60*60));
+
+        // Check if it's been at least 90 days since the custom date
+        if ($days_difference >= 90) {
+            return '<h2 class="elementor-heading-title elementor-size-default"></h2>';
+        }
+
+        $custom_date_int = (int)date('Ymd', $custom_date);
+        $today_date_int = (int)date('Ymd', $today);
+        $day = date('j', $custom_date);
+        $month_num = (int)date('n', $custom_date);
+        
+        if ($cf_mostra_anno == 1) {
+            $year = date('Y', $custom_date);
+        } else {
+            $year = "";
+        }
 
         if ($custom_date_int > $today_date_int) {
-			if ($day == 1 || $day == 8 || $day == 11 ) {
-				return sprintf('<h2 class="elementor-heading-title elementor-size-default">Dall\'%d %s %s al cinema</h2>', $day, $italian_months[$month_num],$year);
-			}
-			else {
-				return sprintf('<h2 class="elementor-heading-title elementor-size-default">Dal %d %s %s al cinema</h2>', $day, $italian_months[$month_num],$year);
-			}
-
+            if ($day == 1 || $day == 8 || $day == 11) {
+                return sprintf('<h2 class="elementor-heading-title elementor-size-default">Dall\'%d %s %s al cinema</h2>', $day, $italian_months[$month_num], $year);
+            } else {
+                return sprintf('<h2 class="elementor-heading-title elementor-size-default">Dal %d %s %s al cinema</h2>', $day, $italian_months[$month_num], $year);
+            }
         } elseif ($custom_date_int == $today_date_int) {
             return '<h2 class="elementor-heading-title elementor-size-default">Da oggi al cinema</h2>';
         } else {
