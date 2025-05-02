250d3b40044502af1513d0d71343f9c0d7a0127a
meksONE <meksone@gmail.com>
2025-05-02 18:33:56 +0200

    wp-config default settings

---
+++ wp-config/wp-config_defaults.php
@@ -0,0 +1,8 @@
+<?php
+
+/* Custom wp-config settings*/
+
+define( 'WP_DEFAULT_THEME', 'XXX' );
+define( 'CORE_UPGRADE_SKIP_NEW_BUNDLED' , true );
+define( 'CODE_SNIPPETS_SAFE_MODE', false );
+define( 'WP_MEMORY_LIMIT', '512M' );
